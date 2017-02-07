1d00082bd527b8bea5030ff2542ec72492ead90a
kedzilla <xavierkedzi@gmail.com>
2017-02-06 22:45:23 -0500

    Removed unused code

--- mqlservice/src/test/com/multitel/mql/mqlservice/domain/keyword/KeywordTest.java
+++ mqlservice/src/test/com/multitel/mql/mqlservice/domain/keyword/KeywordTest.java
@@ -15,14 +15,8 @@ import static org.mockito.Mockito.when;
 public class KeywordTest {
 
     private final String SITE = "SITE";
-    private final String NAME = "NAME";
-    private final String ADDRESS = "ADDRESS";
     private final String CITY = "CITY";
-    private final String ZIPCODE = "ZIPCODE";
     private final String EQUIPEMENT = "EQUIPEMENT";
-    private final String SERIAL_NO = "SERIAL_NO";
-    private final String DATE_IN_SERVICE = "DATE_IN_SERVICE";
-    private final String WARRANTY_END = "WARRANTY_END";
 
     private KeywordFactory keywordFactory;
 
