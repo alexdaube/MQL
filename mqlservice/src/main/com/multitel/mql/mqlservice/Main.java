fb0570824538aabc6d4a40471850c21177cd4421
Alex Aube <alexandre.desilets-aube.1@ulaval.ca>
2017-02-07 16:33:19 -0500

    Basic query endpoint

--- mqlservice/src/main/com/multitel/mql/mqlservice/Main.java
+++ mqlservice/src/main/com/multitel/mql/mqlservice/Main.java
@@ -2,6 +2,7 @@ package com.multitel.mql.mqlservice;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        QueryController queryController = new QueryController();
+        queryController.initializeEndPoints();
     }
 }
---
+++ mqlservice/src/main/com/multitel/mql/mqlservice/QueryController.java
@@ -0,0 +1,12 @@
+package com.multitel.mql.mqlservice;
+
+import com.google.gson.Gson;
+import static spark.Spark.get;
+
+public class QueryController {
+
+    public void initializeEndPoints() {
+        get("/query/:query", (request, response) ->
+                "Query for now: " + request.params(":query"), new Gson()::toJson);
+    }
+}
