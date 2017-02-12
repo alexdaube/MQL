bbabdbd174b61b847132faa6c608f25dde4c7e3e
kedzilla <xavierkedzi@gmail.com>
2017-02-11 23:27:59 -0500

    Refactored startup routine

--- mqlservice/src/main/Main.java
+++ mqlservice/src/main/Main.java
@@ -1,12 +1,30 @@
+import domain.keyword.Keyword;
+import domain.keyword.KeywordRepository;
+import infrastructure.InMemoryKeywordRepository;
+import infrastructure.KeywordDevDataFactory;
 import persistence.SQLHelper;
 import persistence.SQLiteHelper;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        initKeywordRepositoryWithDevData(new KeywordDevDataFactory());
+        initDatabaseConnection(new SQLiteHelper());
+        initServer(new QueryController());
+    }
 
-        SQLHelper sqlHelper = new SQLiteHelper();
+    private static void initKeywordRepositoryWithDevData(KeywordDevDataFactory keywordDevDataFactory) {
+        KeywordRepository keywordRepository = new InMemoryKeywordRepository();
+        List<Keyword> keywords = keywordDevDataFactory.createStubKeywords();
+
+        for (Keyword keyword : keywords) {
+            keywordRepository.create(keyword);
+        }
+    }
+
+    private static void initDatabaseConnection(SQLHelper sqlHelper) {
         try {
             sqlHelper.startConnection();
         } catch (SQLException e) {
@@ -14,8 +32,9 @@ public class Main {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+    }
 
-        QueryController queryController = new QueryController();
+    private static void initServer(QueryController queryController) {
         queryController.initializeEndPoints();
     }
 }
