6294b2267f9934f24e5635cc1b94e1c6dfe9a285
Alex Aube <alexandre.desilets-aube.1@ulaval.ca>
2017-03-28 15:10:14 -0400

    Mongo + express Exploration

--- cms/api/src/router.js
+++ cms/api/src/router.js
@@ -1,3 +1,31 @@
-module.exports = function(app) {
-    app.post('/');
+const mongodb = require("mongodb");
+
+const Server = mongodb.Server;
+const Db = mongodb.Db;
+const BSON = mongodb.BSONPure;
+
+const server = new Server('localhost', 27017, {auto_reconnect: true});
+db = new Db('mql', server, {safe: true});
+
+db.open((err, db) => {
+    if (!err) {
+        console.log("Connected to db");
+    }
+});
+
+module.exports = function (app) {
+    app.get('/entities', (req, res) => {
+        db.collection('entities').find().toArray((error, items) => {
+            res.send(items);
+        });
+    });
+    app.post('/entities', (req, res) => {
+        const entities = req.body.entities;
+        db.collection('entities').remove({});
+        db.collection('entities').insert(entities, function (error, record) {
+            if (error) throw error;
+            console.log("data saved");
+            res.status(200).send(record);
+        });
+    });
 };
