91f1fdd4436c73246abce7b5926ec2c66d129af3
Alex Aube <alexandre.desilets-aube.1@ulaval.ca>
2017-04-03 18:25:42 -0400

    Clean up the css

--- demo/src/containers/SearchBar.js
+++ demo/src/containers/SearchBar.js
@@ -9,20 +9,18 @@ import {getQueryKeywordBadgeDetails} from "../utils/badge";
 
 
 const theme = {
-    container: 'searchBarContainer', // 'react-autosuggest__container'
-    containerOpen: 'react-autosuggest__container--open',
-    input: 'searchBarInput form-control', // 'react-autosuggest__input'
-    inputOpen:                'react-autosuggest__input--open',
-    inputFocused:             'react-autosuggest__input--focused',
-    suggestionsContainer:     'react-autosuggest__suggestions-container',
-    suggestionsContainerOpen: 'react-autosuggest__suggestions-container--open',
-    suggestionsList:          'react-autosuggest__suggestions-list',
-    suggestion:               'react-autosuggest__suggestion',
-    suggestionFirst:          'react-autosuggest__suggestion--first',
-    suggestionHighlighted:    'react-autosuggest__suggestion--highlighted',
-    sectionContainer:         'react-autosuggest__section-container',
-    sectionContainerFirst:    'react-autosuggest__section-container--first',
-    sectionTitle:             'react-autosuggest__section-title'
+    container: 'searchBarContainer',
+    input: 'searchBarInput form-control',
+    inputOpen:                'searchBarInputOpen',
+    inputFocused:             'searchBarInputFocused',
+    suggestionsContainer:     'searchBarSuggestionsContainer',
+    suggestionsContainerOpen: 'searchBarSuggestionsContainerOpen',
+    suggestionsList:          'searchBarSuggestionsList',
+    suggestion:               'searchBarSuggestion',
+    suggestionHighlighted:    'searchBarSuggestionHighlighted',
+    sectionContainer:         'searchBarSectionContainer',
+    sectionContainerFirst:    'searchBarSectionContainerFirst',
+    sectionTitle:             'searchBarSectionTitle'
 };
 
 const renderSectionTitle = (section) => {
