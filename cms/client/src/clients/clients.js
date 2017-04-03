9b3f63dfdb8d8809df1762f2a313bb528df54b4c
Nicolas Cotton <nicolas.cotton.2@ulaval.ca>
2017-04-03 14:19:34 -0400

    chaning port

--- cms/client/src/clients/clients.js
+++ cms/client/src/clients/clients.js
@@ -6,13 +6,13 @@ import JunctionsClient from "./JunctionsClient";
 import TablesClient from "./TablesClient";
 
 //const tablesClient = new InMemoryKeywordsClient();
-const tablesClient = new TablesClient("http://localhost:4000/tables");
+const tablesClient = new TablesClient("http://localhost:3000/tables");
 
 //const junctionsClient = new InMemoryJunctionsClient();
-const junctionsClient = new JunctionsClient("http://localhost:4000/junctions");
+const junctionsClient = new JunctionsClient("http://localhost:3000/junctions");
 
 //const operatorsClient = new InMemoryOperatorsClient();
-const operatorsClient = new OperatorsClient("http://localhost:4000/operators");
+const operatorsClient = new OperatorsClient("http://localhost:3000/operators");
 
 module.exports = {
     tablesClient,
